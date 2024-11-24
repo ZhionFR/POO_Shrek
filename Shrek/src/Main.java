f9b3b92b655c4c4605ca600ba34db6d0e744af3d
ZhionFR <163144861+ZhionFR@users.noreply.github.com>
2024-11-24 17:19:24 +0100

    Setup

---
+++ Shrek/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Veuillez saisir un mot :");
+            String str = sc.nextLine();
+            String arg[] = str.split(" ");
+            System.out.println("Vous avez saisi en premier argument : " + arg[0]);
+            System.out.println("Vous avez saisi en deuxieme argument : " + arg[1]);
+
+            //CurrentLocation
+
+            switch(arg[0]) {
+
+                case "GO" :
+                    if (arg[1] == "MARAIS") {
+
+                        //Changer la location.
+                    }
+            }
+    }
+}
