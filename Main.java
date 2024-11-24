cd56bc8e790c30123b89c1bd1604c35b8f958ae6
Mageallan <alexandremimizan@gmail.com>
2024-11-24 17:14:30 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package Shrek;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+		String arg[] = str.split(" ");
+		System.out.println("Vous avez saisi en premier argument : " + arg[0]);
+		System.out.println("Vous avez saisi en deuxieme argument : " + arg[1]);
+		
+		//CurrentLocation
+		
+		switch(arg[0]) {
+		
+		case "GO" : 
+			if (arg[1] == "MARAIS") {
+				
+				//Changer la location.
+			}
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
